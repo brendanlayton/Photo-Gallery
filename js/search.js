f586d93f0f0ef9f04518fa0be230e0341373d4b2
brendanlayton <brendanlayton@gmail.com>
2016-05-10 21:41:16 -0300

    Project 4 - Added Initial Files
    Added the initial HTML, CSS, JS and image files for the Photo Gallery
    project.

---
+++ js/search.js
@@ -0,0 +1,19 @@
+/* Instructions
+
+1. Create a web page using HTML and CSS. The page should have a title, a search box, and a place where a photo gallery will be placed. Make sure it is responsive.
+
+2. Add the provided images to the gallery using the design in the gallery_mockup.png file.
+
+3. Find a jQuery plugin for creating a photo gallery or write your own script. The gallery must include the ability to click on photos and view them in a lightbox (see the photo_lightbox.png file for the design). The gallery should also include support for additional media types like YouTube videos.
+
+4. Add text captions to the images when viewed in the lightbox. See the photo_lightbox.png file for the design.
+
+5. Add back and forward buttons when the lightbox is visible to switch between photos. Also, add keyboard navigation for browsing photos.
+
+6. Implement the search box at the top of the page that filters photos based on the captions. The photos should filter in real-time as you type. Add animation effects when filtering the gallery of photos.
+
+7. Make sure to check your code is valid by running it through an HTML and CSS validator.
+
+8. You should also check for issues with your JavaScript code using JSHint, linked in the Project Resources.
+
+*/
